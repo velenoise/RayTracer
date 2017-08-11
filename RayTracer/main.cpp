f5996506ffc0d2a712a4a8dcdb5d5c030bca9a56
Caio Velenosi <caio.velenosi@gmail.com>
2017-08-11 11:19:19 -0300

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by caiovelenosi on 11/08/17.
+//  Copyright © 2017 caiovelenosi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
